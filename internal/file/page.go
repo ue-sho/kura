1050242c3068d561064e81503c362324c50752d6
ue-sho <tssu45@gmail.com>
2024-11-07 07:14:05 +0900

    feat: Enhance Page struct with additional data types and methods

--- internal/file/page.go
+++ internal/file/page.go
@@ -2,20 +2,28 @@ package file
 
 import (
 	"encoding/binary"
+	"time"
 )
 
 type Page struct {
 	byteBuffer []byte
 }
 
-const int32Size = 4 // int32 is 4 bytes
+const (
+	int32Size = 4 // int32 is 4 bytes
+	int16Size = 2 // int16 is 2 bytes
+	boolSize  = 1 // boolean is 1 byte
+	dateSize  = 8 // date represented as int64 (8 bytes for Unix timestamp)
+)
 
+// NewPage creates a new page with the specified block size
 func NewPage(blockSize int64) *Page {
 	return &Page{
 		byteBuffer: make([]byte, blockSize),
 	}
 }
 
+// NewPageFromBytes creates a page from an existing byte array
 func NewPageFromBytes(b []byte) *Page {
 	return &Page{
 		byteBuffer: b,
@@ -27,7 +35,41 @@ func (p *Page) GetInt(offset int) int32 {
 }
 
 func (p *Page) SetInt(offset int, val int32) {
+	// Performance: Avoids overhead by skipping size checks on each write in high-frequency DB access
+	// Caller responsibility: Client code should ensure data fits, improving system efficiency
+	// if len(p.byteBuffer) < offset+int32Size {
+	// 	return
+	// }
 	binary.LittleEndian.PutUint32(p.byteBuffer[offset:offset+int32Size], uint32(val))
+}
+
+func (p *Page) GetShort(offset int) int16 {
+	return int16(binary.LittleEndian.Uint16(p.byteBuffer[offset : offset+int16Size]))
+}
+
+func (p *Page) SetShort(offset int, val int16) {
+	binary.LittleEndian.PutUint16(p.byteBuffer[offset:offset+int16Size], uint16(val))
+}
+
+func (p *Page) GetBool(offset int) bool {
+	return p.byteBuffer[offset] != 0
+}
+
+func (p *Page) SetBool(offset int, val bool) {
+	if val {
+		p.byteBuffer[offset] = 1
+	} else {
+		p.byteBuffer[offset] = 0
+	}
+}
+
+func (p *Page) GetDate(offset int) time.Time {
+	unixTime := int64(binary.LittleEndian.Uint64(p.byteBuffer[offset : offset+dateSize]))
+	return time.Unix(unixTime, 0)
+}
+
+func (p *Page) SetDate(offset int, date time.Time) {
+	binary.LittleEndian.PutUint64(p.byteBuffer[offset:offset+dateSize], uint64(date.Unix()))
 }
 
 func (p *Page) GetBytes(offset int) []byte {
@@ -41,15 +83,24 @@ func (p *Page) SetBytes(offset int, val []byte) {
 }
 
 func (p *Page) GetString(offset int) string {
-	return string(p.GetBytes(offset))
+	var strBytes []byte
+	for i := offset; i < len(p.byteBuffer); i++ {
+		if p.byteBuffer[i] == 0 { // Null terminator found
+			break
+		}
+		strBytes = append(strBytes, p.byteBuffer[i])
+	}
+	return string(strBytes)
 }
 
 func (p *Page) SetString(offset int, val string) {
-	p.SetBytes(offset, []byte(val))
+	copy(p.byteBuffer[offset:], []byte(val))
+	p.byteBuffer[offset+len(val)] = 0 // Append null terminator
 }
 
+// MaxLength calculates the maximum number of bytes needed to store a string of a given length
 func MaxLength(strlen int) int {
-	return int32Size + strlen
+	return strlen + 1 // Extra byte for the null terminator
 }
 
 func (p *Page) contents() []byte {
